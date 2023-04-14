63bb280808580a1e45bba030f0a097226838fb98
cengizelif <cenelif@gmail.com>
2023-04-14 14:39:09 +0300

    Add project files.

---
+++ MVC_AjaxHelperMetotlari/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using MVC_AjaxHelperMetotlari.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_AjaxHelperMetotlari.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<string> veriler = new List<string>() { "Teknoloji", "Sağlık", "Giyim", "Gıda" };
+
+            Session["liste"] = veriler;
+
+            return View();
+        }
+        public PartialViewResult VeriYukle()
+        {
+            List<string> veriler =(List<string>) Session["liste"];
+
+            System.Threading.Thread.Sleep(3000);
+
+            return PartialView("_PartialPageVeriler",veriler);
+        }
+
+        public MvcHtmlString VeriSil(int id)
+        {
+            List<string> veriler = (List<string>)Session["liste"];
+            veriler.RemoveAt(id);
+
+            Session["liste"] = veriler;
+
+            System.Threading.Thread.Sleep(2000);
+
+            return MvcHtmlString.Empty;
+        }
+
+        public ActionResult Index2()
+        {
+            List<Kisi> kisiler = new List<Kisi>();
+
+            if (Session["kisiler"] != null)
+            {
+                kisiler = (List<Kisi>)Session["kisiler"];
+            }
+
+            ViewBag.kisiler = kisiler;
+            return View(new Kisi());
+        }
+
+        [HttpPost]
+        public PartialViewResult Index2(Kisi kisi)
+        {
+            List<Kisi> kisiler = null;
+    
+            if (Session["kisiler"]!=null)
+            {
+                kisiler = (List<Kisi>)Session["kisiler"];
+            }
+            else
+            {
+                kisiler = new List<Kisi>();
+            }
+
+            kisi.Id = Guid.NewGuid();
+            kisiler.Add(kisi);
+
+            Session["kisiler"] = kisiler;
+            System.Threading.Thread.Sleep(2000);
+           
+            return PartialView("_PartialPageKisiler",kisi);
+        }
+
+        public ActionResult Test() 
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC_AjaxHelperMetotlari/Models/Kisi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_AjaxHelperMetotlari.Models
+{
+    public class Kisi
+    {
+        public Guid Id { get; set; }
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+        public string email { get; set; }
+
+    }
+}
